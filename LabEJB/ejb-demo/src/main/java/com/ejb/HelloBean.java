7212754d2eb53f34d57c2e60b0792b85506cc42e
PHS-7 <moreira.phs@alu.ufc.br>
2025-08-04 22:50:13 -0300

    labs: upload

---
+++ LabEJB/ejb-demo/src/main/java/com/ejb/HelloBean.java
@@ -0,0 +1,10 @@
+package com.ejb;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class HelloBean {
+    public String sayHello(String name) {
+        return "Ola " + name + ", este e´ um EJB!";
+    }
+}
